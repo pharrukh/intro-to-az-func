1870560fc985204de83e72c5b683a830890dadfb
pharrukh <farrukh.normuradov@gmail.com>
2020-04-25 13:48:50 +0200

    refactor: format code and add switch

--- Person/index.js
+++ Person/index.js
@@ -1,44 +1,53 @@
-let Persons = [
-    {
-        id: "1",
-        name: "Farrukh"
-    },
-    {
-        id: "2",
-        name: "Mikhail"
-    }
-]
+const azure = require('azure-storage')
 
 module.exports = async function (context, req) {
-    if (req.method === 'GET') {
-        const id = context.bindings.req.params ? context.bindings.req.params.id : null
-        if(id){
-            const result = Persons.find(p=>p.id == id)
-            context.res = {
-                // status: 200, /* Defaults to 200 */
-                body: result
-            };
-        }else{
-            context.res = {
-                // status: 200, /* Defaults to 200 */
-                body: Persons
-            };
-        }
-    }else if(req.method === 'POST'){
-        Persons.push(req.body)
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: Persons
-        };
-    }else if(req.method === 'DELETE'){
-        const id = context.bindings.req.params ? context.bindings.req.params.id : null
-        Persons = Persons.filter(p=>p.id != id)
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: Persons
-        };
-    }
-    else {
-       
-    }
-};
+  switch (req.method) {
+    case 'GET':
+      const personEntity = context.bindings.personEntity
+
+      delete personEntity.PartitionKey
+      delete personEntity.RowKey
+
+      return {
+        body: personEntity,
+      }
+    case 'POST':
+      context.bindings.tableBinding = []
+      const person = req.body
+
+      context.bindings.tableBinding.push({
+        PartitionKey: 'Persons',
+        RowKey: person.id,
+        name: person.name,
+        surname: person.surname,
+        age: person.age,
+      })
+      break
+
+    case 'DELETE':
+      const id = context.bindings.req.params.id
+      await deletePerson(id)
+      break
+      
+    default:
+      throw new Error('Method not allowed')
+  }
+}
+
+function deletePerson(id) {
+  const tableSvc = azure.createTableService()
+  const task = {
+    PartitionKey: { _: 'Persons' },
+    RowKey: { _: id },
+  }
+
+  return new Promise((resolve, reject) => {
+    tableSvc.deleteEntity('Persons', task, function (error, response) {
+      if (!error) {
+        resolve(response)
+      } else {
+        reject(error)
+      }
+    })
+  })
+}
